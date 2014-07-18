6048d4e2dcd45550dd0ac43b53e5042a77389aa4
xilinx <xilinx@huang.(none)>
2014-07-19 06:26:57 +0800

    extend img edge

--- line_buffer/shift_tab_3_3.vhd
+++ line_buffer/shift_tab_3_3.vhd
@@ -20,7 +20,7 @@ port (
 		din : in std_logic_vector(data_width-1 downto 0);
 		stream_on : in std_logic;		
 		r33, r32, r31, r23,r22,r21,r13,r12,r11 : out std_logic_vector(data_width-1 downto 0);
-		pix_data_valid: out std_logic	;
+		pix_data_valid: out std_logic;
 		pix_col : out std_logic_vector(addr_cnt -1 downto 0);
 	    pix_row : out std_logic_vector(addr_cnt -1 downto 0);
 		pix_addr: out std_logic_vector(addr_cnt - 1 downto 0)
@@ -53,10 +53,12 @@ architecture control of shift_tab_3_3 is
 	signal s_col : std_logic_vector(addr_cnt -1 downto 0);
 	signal s_row : std_logic_vector(addr_cnt -1 downto 0);
 	signal s_addr: std_logic_vector(addr_cnt -1 downto 0);
+	signal s_pix_addr: std_logic_vector(addr_cnt -1 downto 0);
 	signal fifo_en_count :integer range 0 to 2097151;  -- 2^21 = 2097152 > 1920*1080 > 1280*720 > 640*480
 	signal alt_tap_full : std_logic;
 	
 	signal s_r33, s_r32, s_r31, s_r23,s_r22,s_r21,s_r13,s_r12,s_r11 : std_logic_vector(data_width-1 downto 0);
+	signal s_f33, s_f32, s_f31, s_f23,s_f22,s_f21,s_f13,s_f12,s_f11 : std_logic_vector(data_width-1 downto 0);
 	signal reg32,reg31,reg22,reg21,reg12,reg11 : std_logic_vector(data_width -1 downto 0) ;
 	signal s_rom_data : std_logic_vector(data_width - 1 downto 0);
 	
@@ -77,7 +79,7 @@ architecture control of shift_tab_3_3 is
 	signal s_img_done : std_logic;
 --	signal s_img_on : std_logic;
 	signal s_stream_on : std_logic;
-
+	signal s_pix_data_valid : std_logic;
 	begin
 	
 		tap1 : fifo 
@@ -123,10 +125,10 @@ architecture control of shift_tab_3_3 is
 		r33 <= s_r33;	r32 <= s_r32;	r31 <= s_r31;
 		r23 <= s_r23;	r22 <= s_r22;	r21 <= s_r21;
 		r13 <= s_r13;	r12 <= s_r12;	r11 <= s_r11;
+		pix_data_valid <= s_pix_data_valid;
+	--	s_pix_data_valid <= s_data_valid and not s_img_done;
 		
-		pix_data_valid <= s_data_valid and not s_img_done;
-		
-		pix_addr <= s_addr;
+		pix_addr <= s_pix_addr;
 		pix_row <= s_row;
 		pix_col <= s_col;
 		
@@ -198,6 +200,21 @@ architecture control of shift_tab_3_3 is
 			end if ;
 		end process;
 		
+		imgdone : process(clk , rst)
+		begin
+			if rst = '0' then 
+				s_pix_data_valid <= '0';
+				s_pix_addr <= (others => '0');
+			elsif rising_edge(clk) then
+				if s_pix_addr < rom_depth - 1 then
+					s_pix_data_valid <= s_data_valid;				
+				else
+					s_pix_data_valid <= '0';	
+				end if;
+				s_pix_addr <= s_addr;
+			end if;
+		end process;
+		
 
 		addr_gen : process(clk, rst)
 		begin
@@ -224,18 +241,68 @@ architecture control of shift_tab_3_3 is
 			end if;
 		end process;
 		
-		imgdone : process(clk , rst)
+		out_fix : process(clk, rst)
 		begin
-			if rst = '0' then 
-				s_img_done <= '0';
-				--s_data_valid_out <= '0';
+			if rst = '0' then
+				s_f33 <= (others => '0');	s_f32 <= (others => '0');  s_f31 <= (others => '0');
+				s_f23 <= (others => '0'); 	s_f22 <= (others => '0');  s_f21 <= (others => '0'); 
+				s_f13 <= (others => '0'); 	s_f12 <= (others => '0');  s_f11 <= (others => '0'); 
 			elsif rising_edge(clk) then
-				if s_addr = rom_depth - 1 then
-					s_img_done <= '1';
+				if s_data_valid = '1' then
+					if(s_col = 0) then
+						if(s_row = 0) then						
+							s_f11 <= s_r22; s_f12 <= s_r22; s_f13 <= s_r23; 
+							s_f21 <= s_r22; s_f22 <= s_r22; s_f23 <= s_r23;
+							s_f31 <= s_r32; s_f32 <= s_r32; s_f33 <= s_r33;	
+							
+						elsif(s_row = img_height - 1)then
+							s_f11 <= s_r12; s_f12 <= s_r12; s_f13 <= s_r13; 
+							s_f21 <= s_r22; s_f22 <= s_r22; s_f23 <= s_r23;
+							s_f31 <= s_r22; s_f32 <= s_r22; s_f33 <= s_r23;							
+						else
+							s_f11 <= s_r12; s_f12 <= s_r12; s_f13 <= s_r13; 
+							s_f21 <= s_r22; s_f22 <= s_r22; s_f23 <= s_r23;
+							s_f31 <= s_r32; s_f32 <= s_r32; s_f33 <= s_r33;					
+						end if;
+					elsif(s_col = img_width - 1) then
+						if(s_row = 0) then
+							s_f11 <= s_r21; s_f12 <= s_r22; s_f13 <= s_r22; 
+							s_f21 <= s_r21; s_f22 <= s_r22; s_f23 <= s_r22;
+							s_f31 <= s_r31; s_f32 <= s_r32; s_f33 <= s_r32;	
+						elsif(s_row = img_height - 1)then
+							s_f11 <= s_r11; s_f12 <= s_r12; s_f13 <= s_r12; 
+							s_f21 <= s_r21; s_f22 <= s_r22; s_f23 <= s_r22;
+							s_f31 <= s_r21; s_f32 <= s_r22; s_f33 <= s_r22;		
+						else	
+							s_f11 <= s_r11; s_f12 <= s_r12; s_f13 <= s_r12; 
+							s_f21 <= s_r21; s_f22 <= s_r22; s_f23 <= s_r22;
+							s_f31 <= s_r31; s_f32 <= s_r32; s_f33 <= s_r32;	
+						end if;
+					else
+						if(s_row = 0) then
+							s_f11 <= s_r21; s_f12 <= s_r22; s_f13 <= s_r23; 
+							s_f21 <= s_r21; s_f22 <= s_r22; s_f23 <= s_r23;
+							s_f31 <= s_r31; s_f32 <= s_r32; s_f33 <= s_r33;	
+						elsif(s_row = img_height - 1)then
+							s_f11 <= s_r11; s_f12 <= s_r12; s_f13 <= s_r13; 
+							s_f21 <= s_r21; s_f22 <= s_r22; s_f23 <= s_r23;
+							s_f31 <= s_r21; s_f32 <= s_r22; s_f33 <= s_r23;	
+						else
+							s_f11 <= s_r11; s_f12 <= s_r12; s_f13 <= s_r13; 
+							s_f21 <= s_r21; s_f22 <= s_r22; s_f23 <= s_r23;
+							s_f31 <= s_r31; s_f32 <= s_r32; s_f33 <= s_r33;		
+						end if;
+						
+					end if;
+				else
+							s_f33 <= (others => '0');	s_f32 <= (others => '0');  s_f31 <= (others => '0');
+							s_f23 <= (others => '0'); 	s_f22 <= (others => '0');  s_f21 <= (others => '0'); 
+							s_f13 <= (others => '0'); 	s_f12 <= (others => '0');  s_f11 <= (others => '0'); 
 				end if;
 			end if;
 		end process;
 		
+
 	
 end control;
 
