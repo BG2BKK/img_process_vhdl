1d2ab7b776e577050b18d5127a97569a77c8891b
xilinx <xilinx@huang.(none)>
2014-07-19 01:06:10 +0800

    fix about last commit

--- line_buffer/rom_tb.vhd
+++ line_buffer/rom_tb.vhd
@@ -31,98 +31,43 @@ architecture behavior of rom_tb is
 	file v8_file: text open write_mode is "v8_result.dat";
 
 	
-    component image_process
-		 
-    port(
-			clk : in std_logic;
-			en : in std_logic;
-			rst : in std_logic;
-			dout : out std_logic_vector(data_width-1 downto 0);
+	component image_process is
+	port (
+		clk : in std_logic;
+		en : in std_logic;
+		rst : in std_logic;
 			
---			u0,u1,u2,u3,u4,u5,u6,u7,u8 : out std_logic_vector(data_width + data_width+ data_width -1 downto 0);
---			v0,v1,v2,v3,v4,v5,v6,v7,v8 : out std_logic_vector(data_width + data_width+ data_width -1 downto 0);
---			
---			u0_valid,u1_valid,u2_valid,u3_valid,u4_valid,u5_valid,u6_valid,u7_valid,u8_valid : out std_logic;
---			v0_valid,v1_valid,v2_valid,v3_valid,v4_valid,v5_valid,v6_valid,v7_valid,v8_valid : out std_logic;
-			
-			u8 : out std_logic_vector(data_width + data_width+ data_width -1 downto 0);
-			v8 : out std_logic_vector(data_width + data_width+ data_width -1 downto 0);
-			u8_valid : out std_logic;
-			v8_valid : out std_logic;
-			
-			simu_edge_gaussian_valid: out std_logic;
-			simu_edge_gaussian_data : out std_logic_vector(data_width - 1 downto 0);
-			simu_edge_gaussian_addr : out std_logic_vector(addr_cnt - 1 downto 0);
+		r33, r32, r31, r23,r22,r21,r13,r12,r11 : out std_logic_vector(data_width-1 downto 0)	
 		
-			simu_gaussian_valid: out std_logic;
-			simu_gaussian_data : out std_logic_vector(data_width - 1 downto 0);
-			simu_gaussian_addr : out std_logic_vector(addr_cnt - 1 downto 0);
-			
-			simu_edge_dete_valid: out std_logic;
-			simu_edge_dete_data : out std_logic_vector(data_width - 1 downto 0);
-			simu_edge_dete_addr : out std_logic_vector(addr_cnt - 1 downto 0)
-        );
-    end component;
+	  );
+	end component;
+
 
    --inputs
-   signal clk : std_logic := '0';
-   signal en : std_logic := '1';
-   signal addr : std_logic_vector(addr_cnt-1 downto 0) := (others => '0');
-   signal rst : std_logic := '0';
+   signal clk : std_logic ;
+   signal en : std_logic  ;
+   signal rst : std_logic ;
 
  	--outputs
-   signal dout : std_logic_vector(data_width-1 downto 0);
-
-	signal  s_simu_gaussian_valid:std_logic;
-	signal	s_simu_gaussian_data : std_logic_vector(data_width - 1 downto 0);
-	signal	s_simu_gaussian_addr : std_logic_vector(addr_cnt - 1 downto 0);
-	
-	signal  s_simu_edge_gaussian_valid:std_logic;
-	signal	s_simu_edge_gaussian_data : std_logic_vector(data_width - 1 downto 0);
-	signal	s_simu_edge_gaussian_addr : std_logic_vector(addr_cnt - 1 downto 0);
-	
-	signal  s_simu_edge_dete_valid:std_logic;
-	signal	s_simu_edge_dete_data : std_logic_vector(data_width - 1 downto 0);
-	signal	s_simu_edge_dete_addr : std_logic_vector(addr_cnt - 1 downto 0);
-	
-	signal  s_u0, s_u1, s_u2, s_u3, s_u4, s_u5, s_u6, s_u7, s_u8 : std_logic_vector(data_width + data_width+ data_width -1 downto 0);
-	signal  s_v0, s_v1, s_v2, s_v3, s_v4, s_v5, s_v6, s_v7, s_v8 : std_logic_vector(data_width + data_width+ data_width -1 downto 0);
-	signal 	s_u0_valid, s_u1_valid, s_u2_valid, s_u3_valid, s_u4_valid, s_u5_valid, s_u6_valid, s_u7_valid, s_u8_valid : std_logic;
+	signal s_r33, s_r32, s_r31: std_logic_vector(data_width-1 downto 0);
+	signal s_r23, s_r22, s_r21: std_logic_vector(data_width-1 downto 0);
+	signal s_r13, s_r12, s_r11: std_logic_vector(data_width-1 downto 0);
    -- clock period definitions
    constant clk_period : time := 10 ns;
 
 begin
- 
-	-- instantiate the unit under test (uut)
-   med: image_process port map (
+	
+   med: image_process
+   port map (
         clk => clk,
         en => en,
         rst => rst,
-		dout => dout,
-		
---		u0 => s_u0,				u1 => s_u1,					u2 => s_u2,					u3 => s_u3,					u4 => s_u4,	
---		v0 => s_v0,             v1 => s_v1,                 v2 => s_v2,                 v3 => s_v3,                 v4 => s_v4, 
---		u0_valid => s_u0_valid, u1_valid => s_u1_valid,     u2_valid => s_u2_valid,     u3_valid => s_u3_valid,     u4_valid => s_u4_valid,
---		
---		u5 => s_u5,				u6 => s_u6,					u7 => s_u7,					
---		v5 => s_v5,             v6 => s_v6,                 v7 => s_v7,                 
---		u5_valid => s_u5_valid, u6_valid => s_u6_valid,     u7_valid => s_u7_valid,     
-		
-		u8 => s_u8,	
-		v8 => s_v8, 
-		u8_valid => s_u8_valid,		
-
-		simu_edge_gaussian_valid  =>  s_simu_edge_gaussian_valid  ,
-		simu_edge_gaussian_data   =>  s_simu_edge_gaussian_data   ,
-		simu_edge_gaussian_addr   =>  s_simu_edge_gaussian_addr   ,
-		
-		simu_gaussian_valid => s_simu_gaussian_valid,
-		simu_gaussian_data => s_simu_gaussian_data,
-		simu_gaussian_addr => s_simu_gaussian_addr,
-		
-		simu_edge_dete_valid => s_simu_edge_dete_valid,
-		simu_edge_dete_data =>  s_simu_edge_dete_data,
-		simu_edge_dete_addr =>  s_simu_edge_dete_addr
+			
+		r33	=>	s_r33, 	r32	=>	s_r32,	r31	=>	s_r31,
+		r23	=>	s_r23, 	r22	=>	s_r22,	r21 =>  s_r21,
+		r13	=>	s_r13, 	r12	=>	s_r12,	r11 =>  s_r11
+				
+			
         );
 
    -- clock process definitions
@@ -143,52 +88,35 @@ begin
 		wait;
 	end process;
 	
-	--gaussian_write : process(s_simu_gaussian_valid,s_simu_gaussian_addr)
-	--	variable outline : line;
-	--begin
-	--	if s_simu_gaussian_valid = '1' then
-	--		write(outline, s_simu_gaussian_data);
-	--		writeline(gaussian_file, outline);
-	--	end if;
-	--end process;
-	--
-	--edge_dete_write : process(s_simu_edge_dete_valid,s_simu_edge_dete_addr)
-	--	variable outline : line;
-	--begin
-	--	if s_simu_edge_dete_valid = '1' then
-	--		write(outline, s_simu_edge_dete_data);
-	--		writeline(edge_dete_file, outline);
-	--	end if;
-	--end process;
-	--
-	--edge_gaussian_write : process(s_simu_edge_gaussian_valid,s_simu_edge_gaussian_addr)
-	--	variable outline : line;
-	--begin
-	--	if s_simu_edge_gaussian_valid = '1' then
-	--		write(outline, s_simu_edge_gaussian_data);
-	--		writeline(edge_gaussian_file, outline);
-	--	end if;
-	--end process;
-    --
-	--
-	--uv8_write : process(clk,s_u8_valid,s_simu_gaussian_addr)
-	--	variable uline,vline : line;
-	--	variable v_u ,v_v: std_logic_vector(s_u8'range);
-	--begin
-	--	if rising_edge(clk) then
-	--		if s_u8_valid = '1' then
-	--			v_u := std_logic_vector(abs(signed(s_u8)));
-	--			v_v := std_logic_vector(abs(signed(s_v8)));
-	--			
-	--			write(uline, str(conv_integer(s_u8))&"    "&str(conv_integer(v_u)));
-	--			writeline(u8_file, uline);			
-    --
-	--			write(vline, str(conv_integer(s_v8))&"    "&str(conv_integer(v_v)));				
-	--			writeline(v8_file, vline);
-	--		end if;
-	--	end if;
-	--end process;
 	
+	
+--	gaussian_write : process(s_simu_gaussian_valid,s_simu_gaussian_addr)
+--		variable outline : line;
+--	begin
+--		if s_simu_gaussian_valid = '1' then
+--			write(outline, s_simu_gaussian_data);
+--			writeline(gaussian_file, outline);
+--		end if;
+--	end process;
+--	
+--	edge_dete_write : process(s_simu_edge_dete_valid,s_simu_edge_dete_addr)
+--		variable outline : line;
+--	begin
+--		if s_simu_edge_dete_valid = '1' then
+--			write(outline, s_simu_edge_dete_data);
+--			writeline(edge_dete_file, outline);
+--		end if;
+--	end process;
+--	
+--	edge_gaussian_write : process(s_simu_edge_gaussian_valid,s_simu_edge_gaussian_addr)
+--		variable outline : line;
+--	begin
+--		if s_simu_edge_gaussian_valid = '1' then
+--			write(outline, s_simu_edge_gaussian_data);
+--			writeline(edge_gaussian_file, outline);
+--		end if;
+--	end process;
+
 
 
 end;
--- line_buffer/top.vhd
+++ line_buffer/top.vhd
@@ -6,63 +6,50 @@ use ieee.numeric_std.all;
 
 entity image_process is
 generic (
-			constant data_width : integer := 8;
-            constant addr_cnt	: integer := 20	;
-			constant img_width	: integer := 320;
-			constant img_height	: integer := 240;
-			constant rom_depth	: integer := 76800;
-			constant alt_tap_2_2	: integer := 319;
-			constant alt_tap_3_3	: integer := 318;
-			constant alt_tap_5_5	: integer := 316;
-			constant window_size_2_2 : integer := 2;
-			constant window_size_3_3 : integer := 3;
-			constant window_size_5_5 : integer := 5
-			
 			--constant data_width : integer := 8;
-            --constant addr_cnt	: integer := 10	;			
-			--constant img_width	: integer := 16;
-			--constant img_height	: integer := 16;
-			--constant rom_depth	: integer := 256;
-			--constant alt_tap_2_2	: integer := 15;
-			--constant alt_tap_3_3	: integer := 14;
-			--constant alt_tap_5_5	: integer := 12;
+            --constant addr_cnt	: integer := 20	;
+			--constant img_width	: integer := 320;
+			--constant img_height	: integer := 240;
+			--constant rom_depth	: integer := 76800;
+			--constant alt_tap_2_2	: integer := 319;
+			--constant alt_tap_3_3	: integer := 318;
+			--constant alt_tap_5_5	: integer := 316;
 			--constant window_size_2_2 : integer := 2;
-			--constant window_size_3_3 : integer := 3; 
+			--constant window_size_3_3 : integer := 3;
 			--constant window_size_5_5 : integer := 5
+			
+			constant data_width : integer := 8;
+            constant addr_cnt	: integer := 10	;			
+			constant img_width	: integer := 16;
+			constant img_height	: integer := 16;
+			constant rom_depth	: integer := 256;
+			constant alt_tap_2_2	: integer := 15;
+			constant alt_tap_3_3	: integer := 14;
+			constant alt_tap_5_5	: integer := 12;
+			constant window_size_2_2 : integer := 2;
+			constant window_size_3_3 : integer := 3; 
+			constant window_size_5_5 : integer := 5
 );
 port (
 		clk : in std_logic;
 		en : in std_logic;
 		rst : in std_logic;
-		dout : out std_logic_vector(data_width-1 downto 0);
-	
---		u0,u1,u2,u3,u4,u5,u6,u7,u8 : out std_logic_vector(data_width + data_width+ data_width -1 downto 0);
---		v0,v1,v2,v3,v4,v5,v6,v7,v8 : out std_logic_vector(data_width + data_width+ data_width -1 downto 0);
---		
---		u0_valid,u1_valid,u2_valid,u3_valid,u4_valid,u5_valid,u6_valid,u7_valid,u8_valid : out std_logic;
---		v0_valid,v1_valid,v2_valid,v3_valid,v4_valid,v5_valid,v6_valid,v7_valid,v8_valid : out std_logic;
+			
+		r33, r32, r31, r23,r22,r21,r13,r12,r11 : out std_logic_vector(data_width-1 downto 0)		
 		
-		u8 : out std_logic_vector(data_width + data_width+ data_width -1 downto 0);
-		v8 : out std_logic_vector(data_width + data_width+ data_width -1 downto 0);
-		u8_valid : out std_logic;
-		v8_valid : out std_logic;
-		
-		simu_edge_gaussian_valid: out std_logic;
-		simu_edge_gaussian_data : out std_logic_vector(data_width - 1 downto 0);
-		simu_edge_gaussian_addr : out std_logic_vector(addr_cnt - 1 downto 0);
-		
-		simu_gaussian_valid: out std_logic;
-		simu_gaussian_data : out std_logic_vector(data_width - 1 downto 0);
-		simu_gaussian_addr : out std_logic_vector(addr_cnt - 1 downto 0);
-		
-		simu_edge_dete_valid: out std_logic;
-		simu_edge_dete_data : out std_logic_vector(data_width - 1 downto 0);
-		simu_edge_dete_addr : out std_logic_vector(addr_cnt - 1 downto 0)
 	  );
 end image_process;
 
 architecture behavior of image_process is
 
+	component sys_ctl is
+	port (
+			clk : in std_logic;
+			en : in std_logic;
+			rst : in std_logic;
+			sys_ctl_en : out std_logic
+		);
+	end component;
 	component rom	
 	generic (
 			data_width : integer ;
@@ -122,41 +109,21 @@ architecture behavior of image_process is
 	end component;
 
 	signal s_ctl_en: std_logic;	
-	signal s_rom_addr : std_logic_vector(addr_cnt -1  downto 0) := (others => '0');
+	signal s_rom_addr, s_pix_addr : std_logic_vector(addr_cnt -1  downto 0) := (others => '0');
+	signal s_r33, s_r32, s_r31: std_logic_vector(data_width-1 downto 0);
+	signal s_r23, s_r22, s_r21: std_logic_vector(data_width-1 downto 0);
+	signal s_r13, s_r12, s_r11: std_logic_vector(data_width-1 downto 0);
 	signal s_rom_out: std_logic_vector(data_width -1 downto 0);
-	
-	signal s_col : std_logic_vector(addr_cnt -1 downto 0);
-	signal s_row : std_logic_vector(addr_cnt -1 downto 0);	
-	signal s_pix_addr: std_logic_vector(addr_cnt -1 downto 0);	
-	signal s_img_on : std_logic;
-
-	signal edge_gaussian_out_addr: std_logic_vector(addr_cnt -1 downto 0);
-	signal edge_gaussian_out_data: std_logic_vector(data_width -1 downto 0);	
-	signal edge_gaussian_out_valid: std_logic;		
-	
-	signal gaussian_out_addr: std_logic_vector(addr_cnt -1 downto 0);
-	signal gaussian_out_data: std_logic_vector(data_width -1 downto 0);	
-	signal gaussian_out_valid: std_logic;
-
-	signal edge_dete_out_addr: std_logic_vector(addr_cnt -1 downto 0);
-	signal edge_dete_out_data: std_logic_vector(data_width -1 downto 0);	
-	signal edge_dete_out_valid: std_logic;	
-	
-	signal s_data_addr : std_logic_vector(addr_cnt - 1 downto 0);
+	signal s_stream_on : std_logic;
 	signal s_data_valid : std_logic;
 	
-	--img_0 and img_1
-	signal s0_r22,s0_r21,s0_r12,s0_r11 : std_logic_vector(data_width -1 downto 0) ;
-	signal s_img_0_addr : std_logic_vector(addr_cnt -1  downto 0) ;
-	signal s_img_0_on : std_logic;
-	signal s_img_0_out: std_logic_vector(data_width -1 downto 0);
-	signal s_img_0_data_addr : std_logic_vector(addr_cnt - 1 downto 0);
-	signal s_img_0_data_valid : std_logic;
-	
-
 begin
 	
-	dout <= s_Ix(dout'range);
+	
+	r33	<=	s_r33;	r32	<=	s_r32;	 r31 <=	 s_r31;
+	r23	<=	s_r23;  r22	<=	s_r22;   r21 <=  s_r21;
+	r13	<=	s_r13;  r12	<=	s_r12;   r11 <=  s_r11;
+	
 	sysctl : sys_ctl
 	port map (
           clk => clk,
@@ -174,8 +141,8 @@ begin
 	port map (
           clk => clk,
           en => s_ctl_en,
-          addr => s_img_0_addr,
-		  data => s_img_0_out
+          addr => s_rom_addr,
+		  data => s_rom_out
     );
 	img_read : rom_ctl
 	generic map(
@@ -189,11 +156,40 @@ begin
 		clk => clk,
 		rst => rst,
 		en 	=> s_ctl_en,
-		stream_on => s_img_0_on,
+		stream_on => s_stream_on,
 --		col	=> s_col,
 --		row => s_row,
 --		pix_addr => s_pix_addr,
-		addr => s_img_0_addr
+		addr => s_rom_addr
+	);
+	
+	window_3_3: shift_tab_3_3
+	generic map(
+			data_width	=> data_width,
+			addr_cnt	=> addr_cnt,
+			img_width	=> img_width,
+			img_height 	=> img_height,
+			rom_depth	=> rom_depth,
+			alt_tap		=> alt_tap_3_3,
+			window_size	=> window_size_3_3
+	)
+	port map(
+		clk => clk,
+		rst => rst,
+		en 	=> s_ctl_en,
+		din => s_rom_out,
+		stream_on => s_stream_on,
+		r33	=>	s_r33, 
+		r23	=>	s_r23, 		
+		r13	=>	s_r13, 		
+		r32	=>	s_r32, 
+		r22	=>	s_r22, 
+		r12	=>	s_r12, 
+		r31	=>	s_r31,
+		r21 =>  s_r21,
+		r11 =>  s_r11,
+		pix_data_valid => s_data_valid,
+		pix_addr => s_pix_addr			
 	);
 
 	
